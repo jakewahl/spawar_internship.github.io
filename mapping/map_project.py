534a8cc622f30562803b2c39159b40149fafd5f8
jakewahl <wahlj@san.rr.com>
2015-07-08 16:16:32 -0700

    database sql update

--- mapping/map_project.py
+++ mapping/map_project.py
@@ -29,7 +29,24 @@ if __name__ == '__main__':
     geolist_to_jsvar(l)
 
 
+def make_query_str(table, filters):
+    '''
 
+    :param table: name of table in database
+    :param filters: dictionary where the key is the column name and the value is a tuple of strings to be combined into a SQL condition
+    :return: a string that is valid SQL syntax to act on a database
+    '''
+
+    s = "SELECT unixtime, lat, lon, crs, spd, MMSI, SHIPNAME"
+
+    def make_sql(conditions):
+        string = ""
+        for k, v in conditions.iteritems():
+            string += " AND {col} {oper} '{val1}' {_and} '{val2}'".format(col=k, oper=v[0], val=v[1], _and=v[2], val2=v[3])
+        return string
+
+    s += " FROM {table} WHERE 1{filt}".format(table=table, filt=make_sql(filters))
+    return s
 
 
 
